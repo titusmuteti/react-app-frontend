1a2ecfe5ae3ff745aa64bef5ad10cbb433867165
Titus Mumo <titusmumo@gmail.com>
2022-10-04 16:23:53 +0300

    install tailwind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-import { useEffect } from "react";
 
 function App() {
-  useEffect(()=> {
-    fetch("https://fierce-ridge-31455.herokuapp.com/products")
-    .then(res => res.json())
-    .then(res => console.log(res))
-  },[])
-  return 
 }
 
 export default App;
