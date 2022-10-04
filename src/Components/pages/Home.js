3b179b6e1b948c62543ade2e24fdc098eed4bb18
Titus Mumo <titusmumo@gmail.com>
2022-10-05 00:35:40 +0300

    home page

--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='home'>
-      <h1 className='text-white text-9xl italic'>Your One Stop shop</h1>
-      <label className='checkbox-wrapper'>
+      <h1 className='text-white text-9xl italic py-10'>Your One Stop shop</h1>
+      <label className='checkbox-wrapper text-3xl'>
         <input type={"checkbox"}/>
         Check to receive notification on latest products
       </label>
