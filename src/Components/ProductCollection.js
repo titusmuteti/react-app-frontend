e5ea0467708131963db32f06315274d2b247cc4c
Titus Mumo <titusmumo@gmail.com>
2022-10-05 00:57:36 +0300

    product list

--- src/Components/ProductCollection.js
+++ src/Components/ProductCollection.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import ProductCard from './ProductCard'
 
-const ProductCollection = () => {
+const ProductCollection = ({products}) => {
+  //console.log(products);
+  const displayProducts = products.map(product => {
+    return (
+      <ProductCard />
+    )
+  })
   return (
-    <div>ProductCollection</div>
+    <div className='grid-cols-4'>
+      <div className='grid-rows-5'>
+        ProductCollection
+        {displayProducts}
+      </div>
+    </div>
   )
 }
 
--- src/Components/pages/Products.js
+++ src/Components/pages/Products.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductCollection from '../ProductCollection';
 
 const Products = () => {
+const [productsList, setProductsList] = useState([]);
+
+useEffect(()=> {
+  fetch("https://fierce-ridge-31455.herokuapp.com/products")
+  .then(res => res.json())
+  .then(products => setProductsList(products))
+})
   return (
     <div>
-      <ProductCollection />
+      <ProductCollection products={productsList}/>
     </div>
   )
 }
