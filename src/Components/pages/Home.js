eef97cab983162978087bc3c05506ad8e8254685
Titus Mumo <titusmumo@gmail.com>
2022-10-04 23:34:56 +0300

    redirect file edit

--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -1,18 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 const Home = () => {
-  const [img, setImg] = useState([])
-  useEffect(()=> {
-    fetch("https://fierce-ridge-31455.herokuapp.com/products")
-    .then(res => res.json())
-    .then(res => setImg(res))
-  })
   return (
     <div>
-      <h1 className='text-blue' >The place to shop</h1>
-      {img.map(img=> {
-        return (<img src={img.image} />)
-      })}
+      <h1>The place to shop</h1>
     </div>
   )
 }
