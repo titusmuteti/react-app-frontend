aab4bf46cbcc40298d23f02bbc8265d92c7f85af
Titus Mumo <titusmumo@gmail.com>
2022-10-04 22:40:20 +0300

    test

--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
+  const [img, setImg] = useState([])
+  useEffect(()=> {
+    fetch("https://fierce-ridge-31455.herokuapp.com/products")
+    .then(res => res.json())
+    .then(res => setImg(res))
+  })
   return (
     <div>
       <h1 className='text-blue' >The place to shop</h1>
+      {img.map(img=> {
+        return <img src={img.image} />
+      })}
     </div>
   )
 }
