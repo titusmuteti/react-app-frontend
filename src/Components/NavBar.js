3f07413f047fbdb7954e50507aaccd0ce021843c
Titus Mumo <titusmumo@gmail.com>
2022-10-04 18:37:24 +0300

    add react icons

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,7 +2,22 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <nav className='navbar navbar-inverse'></nav>
+    <nav className='nav'>
+        <a href='/' className='Class-Collection'>Class Collecting</a>
+        <ul>
+            <li>
+                <a href='/home' className=''>Home</a>
+            </li>
+
+            <li>
+                <a href='/reviews'>Reviews</a>
+            </li>
+
+            <li>
+                <a href='/about'>About</a>
+            </li>
+        </ul>
+    </nav>
   )
 }
 
