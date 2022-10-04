f9b5f5199fb47d6fb358eaa9380e423e4e2081bc
Titus Mumo <titusmumo@gmail.com>
2022-10-04 22:08:42 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
     <NavBar />
       <Switch>
-        <Route exact path={"/home"}>
+        <Route exact path={"src/Components/pages/Home.js"}>
           <Home />
         </Route>
 
