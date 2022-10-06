e41e67e0f891eaf6719ac1a96afc2120266a6667
Titus Mumo <titusmumo@gmail.com>
2022-10-06 16:03:27 +0300

    commit

--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -5,7 +5,6 @@ const Home = () => {
     <div className='home mt-10'>
       <h1 className='text-white text-9xl italic py-10 mt-0 inline-block'>
         <span className='inline-block'>
-          <img className='h-40' src='https://api10.iloveimg.com/thumbnails/g27d4mrsg3ztmnzAgm5d3njAg23j5xnnd8pltn8qsz2nvyz08vvr4A8pbsqp30650pvb8ktAbg2jwkbsfhm32jr87rjrnk071d0347t7jbcqpscj3zp2v872d6htgnywybd7dxwmjxdzAnlkjdnfjgt9bc7llyktj39xlm5b6y8474ky6tsq/fd6bd4a9035c5fe4398d94813610d108c086109e584cdc0b283862a028815ff4.png' alt=''/>
         </span>
         Your One Stop shop
       </h1>
