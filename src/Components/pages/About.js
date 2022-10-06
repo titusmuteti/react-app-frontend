248af127dbe6b450e0c862cab5e893a264ec41f9
Titus Mumo <titusmumo@gmail.com>
2022-10-06 22:38:55 +0300

    add image to form

--- src/Components/pages/About.js
+++ src/Components/pages/About.js
@@ -3,27 +3,28 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      <h1 className='text-4xl mt-20'>Want to market your product with us?</h1>
+      <h1 className='text-4xl mt-20 font-bold'>Want to market your product with us?</h1>
 
       <div className="App">
     <header className="App-header">
     <form >
-            <label >
-          Product Title:
-        </label><br/>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBC-9OSf41AWvLERVefeZdV1FKvWc6SDqdXA&usqp=CAU" alt="" />
+      <label >
+        Product Title:
+      </label><br/>
         <input type="text" className='border-4 border-black' /><br/>
           
-        <label >
-          Price tag:
-        </label><br/>
+      <label >
+        Price tag:
+      </label><br/>
         <input type="number" placeholder='$' className='border-4 border-black' /><br/>
            
-        <label>
-          Image URL:
-        </label><br/>
+      <label>
+        Image URL:
+      </label><br/>
         <input type="text" className='border-4 border-black'/><br/>
          
-        <input type="submit" value="Submit"/>
+      <input type="submit" value="Submit"/>
       </form>
     </header>
     </div>
